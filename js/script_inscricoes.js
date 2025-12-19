5caa69b54d816b86492ca07eba5eb3b9c190ec94
LeoViolet <lleonardoo.silvaa90@gmail.com>
2025-12-19 19:29:24 +0000

    backgrounds adicionados, inscrições salvam

--- js/script_inscricoes.js
+++ js/script_inscricoes.js
@@ -2,6 +2,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     const eventoTitulo = localStorage.getItem('eventoSelecionado');
     const eventoInput = document.getElementById('evento');
+    const id_event = localStorage.getItem('eventoSelecionadoId');
+    const inscricoesSalvas = localStorage.getItem('greenDateInscricoes');
+    var inscricoes = inscricoesSalvas ? JSON.parse(inscricoesSalvas) : {};
     
     if (eventoTitulo && eventoInput) {
         eventoInput.value = eventoTitulo;
@@ -17,13 +20,18 @@ document.addEventListener('DOMContentLoaded', function() {
             const telefone = document.getElementById('tlm').value;
             const evento = document.getElementById('evento').value;
             const info = document.getElementById('info').value;
+            const inscri_info = [nome, email, telefone, evento, info];
             
             if (!evento || evento === "Insira o evento que se deseja inscrever em") {
                 alert('Por favor, selecione um evento primeiro no calendário ou eventos.');
                 return;
             }
             
-            //faz o save aqui!!!
+            //Save das Inscrições
+            if (!inscricoes[id_event]) inscricoes[id_event] = [];
+            inscricoes[id_event].push(inscri_info);
+            localStorage.setItem('greenDateInscricoes', JSON.stringify(inscricoes));
+
             
             console.log('Inscrição enviada:', {
                 nome, email, telefone, evento, info,
